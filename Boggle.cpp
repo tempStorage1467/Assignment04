10975e7cc3515bc6f3af0934a94edc5dce3598cf
Eric Beach <ebeach@google.com>
2013-05-11 14:31:23 -0400

    working version; commented

--- Boggle.cpp
+++ Boggle.cpp
@@ -1,10 +1,9 @@
     /*
  * File: Boggle.cpp
  * ----------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
+ * Name: Eric Beach
+ * Section: Dawson Zhou
  * This file is the main starter file for Assignment #4, Boggle.
- * [TODO: extend the documentation]
  */
  
 #include <iostream>
@@ -38,7 +37,10 @@ const string BIG_BOGGLE_CUBES[25]  = {
 };
 
 ////////// FUNCTION IMPLEMENTATION //////////
-/* Task 1: Cube Setup, Board Drawing, and Cube Shaking */
+/***** Task 1: Cube Setup, Board Drawing, and Cube Shaking *****/
+/*
+ * Convert an array of strings into a vector of strings.
+ */
 Vector<string> arrayToVector(const string str[], int length) {
     Vector<string> vec(length);
     for (int i = 0; i < length; i++) {
@@ -47,6 +49,10 @@ Vector<string> arrayToVector(const string str[], int length) {
     return vec;
 }
 
+/*
+ * Shuffle cubes such that the order of the cubes is random. This helps
+ *   to ensure a random board.
+ */
 Vector<string> shuffleCubes(const string iCubes[], int numCubes) {
     Vector<string> cubes = arrayToVector(iCubes, numCubes);
 
@@ -59,22 +65,28 @@ Vector<string> shuffleCubes(const string iCubes[], int numCubes) {
     return cubes;
 }
 
-// exposedCubeFaces is an output parameter
-// vecBoard is an output parameter
+/*
+ * Process the characters in the shuffled cubes, putting them onto the board
+ *   and populating output parameters.
+ * $exposedCubeFaces and $vecBoard are both output parameters that will be
+ *   populated and used later in the game.
+ */
 void processChars(Vector<string>& cubes, int rows, int cols,
                   Set<char>& exposedCubeFaces,
                   Vector<Vector<char> >& vecBoard) {
-    int charPlaced = 0;
+    int cubeCnt = 0;
     for (int row = 0; row < rows; row++) {
+        // create a vector representing a column of cubes
         Vector<char> colVec;
         for (int col = 0; col < cols; col++) {
-            int cubeFace = randomInteger(0, cubes[charPlaced].size() - 1);
-            // swap elements so that placed char is always at zero element
-            // this enables us to easily iteratr over $cubes and know
-            // what is on the board and where
-            char temp = cubes[charPlaced][cubeFace];
-            cubes[charPlaced][cubeFace] = cubes[charPlaced][0];
-            cubes[charPlaced][0] = temp;
+            int selectedCubeFace = randomInteger(0, cubes[cubeCnt].size() - 1);
+            // swap elements so that the placed char is always at zero element.
+            //   i.e., randomize the char facing up on the cube
+            // placing the randomly selected char in the 0th element
+            //  enables easy iteration over $cubes later on
+            char temp = cubes[cubeCnt][selectedCubeFace];
+            cubes[cubeCnt][selectedCubeFace] = cubes[cubeCnt][0];
+            cubes[cubeCnt][0] = temp;
             
             // store the exposed char into a set for use later in the program
             exposedCubeFaces.add(temp);
@@ -85,20 +97,28 @@ void processChars(Vector<string>& cubes, int rows, int cols,
             
             // place face-up char from cube onto board
             labelCube(row, col, temp);
-            charPlaced++;
+            cubeCnt++;
         }
         vecBoard.add(colVec);
     }
 }
 
+/*
+ * Convert a string to uppercase.
+ */
 void strToUpperCase(string& str) {
     for (int i = 0; i < str.length(); i++) {
         str[i] = toupper(str[i]);
     }
 }
 
-bool isValidCustomBoardConfiguration(string& input, int boardSize) {
-    if (input.size() != boardSize * boardSize) return false;
+/*
+ * Determine whether a specific string input consisting of characters entered
+ *   by the end-user to represent a board configuration is valid. Check
+ *   to ensure that the only used characters are uppercase letters.
+ */
+bool isValidCustomBoardConfiguration(const string& input, int boardLength) {
+    if (input.size() != boardLength * boardLength) return false;
     for (int i = 0; i < input.size(); i++) {
         if (input[i] < 65 || input[i] > 90) {
             return false;
@@ -107,6 +127,11 @@ bool isValidCustomBoardConfiguration(string& input, int boardSize) {
     return true;
 }
 
+/*
+ * Provide instructions to the user on how to enter a manual board
+ *   configuration and then receive the input from the user. Run a function
+ *   to check the input and if invalid, prompt the user again.
+ */
 Vector<string> getManualCubes(const int boardSideLen) {
     cout << "" << endl;
     cout << "Enter a " << (boardSideLen * boardSideLen) << "-character string ";
@@ -114,7 +139,7 @@ Vector<string> getManualCubes(const int boardSideLen) {
     cout << "The first " << boardSideLen << " letters are the cubes on the top";
     cout << " row from left to right, the next " << boardSideLen << "letters";
     cout << " are on the second row, and so on." << endl;
-    
+
     string input;
     while (input != "y" && input != "yes" &&
            input != "n" && input != "no") {
@@ -128,17 +153,23 @@ Vector<string> getManualCubes(const int boardSideLen) {
         }
     }
 
-    // copy each char of input string as a string element of vector
+    // copy each char of user's custom configured board, which is captured
+    //   as an input string, into a string element of vector
     Vector<string> cubes(input.size());
     for (int i = 0; i < input.size(); i++) {
-        // convert char to string safely and put into array
+        // convert char, which represents a cube, to string
+        //   which is put into vector containing all the cubes
         cubes[i] += input[i];
     }
     return cubes;
 }
 
+/*
+ * Ask the end user a boolean (i.e., yes/no) question.
+ */
 bool askBoolQuestion(string preface, string question) {
     cout << endl;
+    // if a preface to the question is supplied, print it
     if (preface.length() > 0) {
         cout << preface << endl;
     }
@@ -151,15 +182,12 @@ bool askBoolQuestion(string preface, string question) {
     return (input == "Y" || input == "YES");
 }
 
-/* Main program */
-
 /*
  * Function: welcome
  * Usage: welcome();
  * -----------------
  * Print out a cheery welcome message.
  */
-
 void welcome() {
     cout << "Welcome!  You're about to play an intense game ";
     cout << "of mind-numbing Boggle.  The good news is that ";
@@ -175,7 +203,6 @@ void welcome() {
  * --------------------------
  * Print out the instructions for the user.
  */
-
 void giveInstructions() {
     cout << endl;
     cout << "The boggle board is a grid onto which I ";
@@ -200,10 +227,14 @@ void giveInstructions() {
     getLine();
 }
 
-
-/* Task 2: Human's Turn (except for finding words on the board) */
-string getNextHumanWord(Set<string>& enteredWords,
-                        Lexicon& dict,
+/***** Task 2: Human's Turn (except for finding words on the board) *****/
+/*
+ * Ask the end user to provide another guess of a word on the board.
+ * Perform basic checking of this guessed word, ensuring that it has not
+ *   been guessed before, it is of valid length, and it is a valid word.
+ */
+string getNextHumanWord(const Set<string>& enteredWords,
+                        const Lexicon& dict,
                         const int minWordLen) {
     string input;
     bool promptAgain = true;
@@ -212,18 +243,20 @@ string getNextHumanWord(Set<string>& enteredWords,
         input = toUpperCase(input);
         if (input == "") break;
         if (enteredWords.contains(input)) {
-            // check if user already entered this word, either as a guess (in
+            // check if user already entered this word as a guess (in
             //   which case we shouldn't waste time checking it again or as
             //   a successful word)
             cout << "You're already guessed that!" << endl;
             continue;
         }
         if (input.size() < minWordLen) {
+            // check if the word is too short
             cout << "I'm sorry, but we have standards." << endl;
             cout << "That word doesn't meet the minimum word length." << endl;
             continue;
         }
         if (!dict.contains(input)) {
+            // check if the word is valid in the dictionary
             cout << "That's not a word!" << endl;
             continue;
         }
@@ -233,12 +266,28 @@ string getNextHumanWord(Set<string>& enteredWords,
 }
 
 /***** Task 3: Find a given word on the board *****/
+/*
+ * A structure to represent a coordinate position on the board.
+ */
 struct Coordinate {
     int colNum;
     int rowNum;
 };
 
-// is word present when starting form a certain location
+/*
+ * Determine whether a sequence of characters (i.e., $word) can be formed
+ *   by starting at a given location (i.e., colN, rowN) and trying all
+ *   adjacent locations that have not previously been explored in this
+ *   recursive branch (which is measured by examining $board for the
+ *   presence of an EXAMINED_CUBE).
+ *
+ * The key recursive insight is that if a word can be formed from a given
+ *   location, then the next character in the string (i.e., word[0])
+ *   needs to be present at a given location and we then explore all the
+ *   adjacent locations for word[1], which we can pass as word.substr(1).
+ *   In other words, if "dime" can be formed from 0,0 then a d needs to be at
+ *   0,0 and we need to be able to form the word "ime" from an adjacent cube.
+ */
 bool isWordPresentFrom(const string& word, Vector<Coordinate>& outputPath,
                        int rowN, int colN, const int boardSideLen,
                        Vector<Vector<char> > board) {
@@ -279,6 +328,12 @@ bool isWordPresentFrom(const string& word, Vector<Coordinate>& outputPath,
     return false;
 }
 
+/*
+ * This is the helper function that sets up the calls to the function
+ *   that does the heart of Task 3 (i.e., isWordPresentFrom).
+ * This function determine whether a word is present when starting from
+ *   every possible location on the board.
+ */
 bool isWordPresent(const string& word, Vector<Coordinate>& outputPath,
                    const Vector<Vector<char> > & board,
                    const int boardSideLen) {
@@ -293,6 +348,18 @@ bool isWordPresent(const string& word, Vector<Coordinate>& outputPath,
     return false;
 }
 
+/*
+ * Determine whether it is possible for a given guessed word to be present
+ *   on the board by checking if it is too long and by checking to ensure
+ *   all the required characters are on the board.
+ * This quick check improves overall efficiency by preventing more expensive
+ *   recursive exploration of the board in search of a word that cannot
+ *   possibly be present. For example, if the word is "computer" but the
+ *   board does not contain an "r", it would be an extremely expensive
+ *   resursive operation to discover this but with a few iterations
+ *   over a set, this function would detect it and prevent the
+ *   expensive recursive operation.
+ */
 bool quickWordPresenceCheck(const string& word, const int maxWordLen,
                             const Set<char>& exposedCubeFaces) {
     if (word.size() > (maxWordLen * maxWordLen)) return false;
@@ -304,6 +371,15 @@ bool quickWordPresenceCheck(const string& word, const int maxWordLen,
 }
 
 /***** Task 4: Find all words on the board (computer's turn) *****/
+/* Find all words that can be formed from a given starting position with a
+ *   given part of a word found thus far (i.e., find words from a given location
+ *   with a given prefix, stored in $soFar).
+ * This function is the heart of Task #4.
+ * The key recursive insight is that all the words that can be formed from
+ *   (colN, rowN) with a stem of $soFar are all the words that can be formed
+ *   from the adjoining locations where $soFar has the current cube's char
+ *   added.
+ */
 void isWordFormedFrom(string soFar, const int colN, const int rowN,
                       Set<string>& foundWords, Vector<Vector<char> > board,
                       const int boardSideLen, const int minWordLen,
@@ -336,6 +412,13 @@ void isWordFormedFrom(string soFar, const int colN, const int rowN,
     }
 }
 
+/*
+ * This is the helper function that sets up the recursive function that
+ *   performs the heart of the work in Task 4.
+ * This function gets all words that can be formed starting from every
+ *   location on the board. It appends all words formed from each location
+ *   into a set.
+ */
 Set<string> getAllWords(Vector<Vector<char> >& vecBoard,
                         const int boardSideLen,
                         const int minWordLen,
@@ -387,7 +470,11 @@ int main() {
                              SIDE_LEN * SIDE_LEN);
     }
 
+    // a set containing all the chars that are facing up on the board.
+    //   this is used to perform quick checking of user guesses.
     Set<char> exposedCubeFaces;
+    
+    // a 2-dimensional vector representing the chars that are shown on the board
     Vector<Vector<char> > vecBoard;
     processChars(cubes, SIDE_LEN, SIDE_LEN, exposedCubeFaces, vecBoard);
     
@@ -420,6 +507,8 @@ int main() {
             cout << "You can't make that word!" << endl;
             continue;
         } else {
+            // guessed word could be found; highlight it on the board and then
+            //   remove the highlighting
             for (int i = 0; i < outPath.size(); i++) {
                 highlightCube(outPath[i].rowNum, outPath[i].colNum, true);
             }
@@ -428,14 +517,17 @@ int main() {
             for (int i = 0; i < outPath.size(); i++) {
                 highlightCube(outPath[i].rowNum, outPath[i].colNum, false);
             }
+            outPath.clear();
         }
-        outPath.clear();
     }
 
     /***** Task 4: Find all words on the board (computer's turn) *****/
+    // find all possible words on the board
     Set<string> allWords = getAllWords(vecBoard, SIDE_LEN,
                                        MIN_WORD_LENGTH, dict);
+    // remove already guessed words
     Set<string> remainingWords = allWords - enteredWords;
+    // print each word onto the screen that the computer found
     foreach(string foundWord in remainingWords) {
         recordWordForPlayer(foundWord, COMPUTER);
     }
